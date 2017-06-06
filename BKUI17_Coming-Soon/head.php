1f26acbe9938e57671e59eeac9fd464fbee3804c
dwi.nanda09 <dwi.nanda@ui.ac.id>
2017-06-06 10:32:47 +0700

    tambahin header ama footer

---
+++ BKUI17_Coming-Soon/head.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link rel="shortcut icon" href="app/images/favicon.png" type="image/x-icon" />
+
+  <link rel="stylesheet" type="text/css" href="vendor/css/font-awesome.min.css"/>
+  <link rel="stylesheet" type="text/css" href="vendor/css/tooltipster.css"/>
+  <link rel="stylesheet" type="text/css" href="vendor/css/tooltipster-shadow.css"/>
+  <link rel="stylesheet" type="text/css" href="vendor/css/remodal.css"/>
+  <link rel="stylesheet" type="text/css" href="vendor/css/remodal-default-theme.css"/>
+  <link rel="stylesheet" type="text/css" href="app/css/styles.css?v=1.3.5"/>
+</head>
+<body>
+
+  <div class="wrapper">
+    <div class="web-content">
+      <div class="social-icon">
+        <a href="https://twitter.com/bkui_official" target="_blank" class="icon tooltip" title="@BKUI_Official"><i class="fa fa-twitter"></i></a>
+        <a href="https://instagram.com/BKUI_Official" target="_blank" class="icon tooltip" title="@BKUI_Official"><i class="fa fa-instagram"></i></a>
+        <a href="tel:+6281298969600"class="icon tooltip" title="+62 812 9896 9600 (Farra)"><i class="fa fa-phone"></i></a>
+        <a href='ticket-manager.php' target="_blank" class="icon tooltip" title="Mohon maaf, penjualan tiket belum dibuka. Penjualan tiket akan segera dibuka pada tanggal 12 Juni 2017"><i class="fa fa-ticket"></i></a>
+      </div>
---
+++ BKUI17_Coming-Soon/ticket-manager.php
@@ -0,0 +1,8 @@
+<?php
+include 'head.php';
+ ?>
+<title>Beli Tiket</title>
+
+ <?php
+ include 'foot.php';
+  ?>
