de1130b6bbc7bdd84cbdd334c9b822aa640d3282
Luthfi Aulia Sulaiman <luthfias@yahoo.com>
2017-06-06 13:19:32 +0700

    Laravel diedit hehe

--- TIKET/resources/views/welcome.blade.php
+++ TIKET/resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ config('app.locale') }}">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Aesthetic Hehe
                 </div>
 
                 <div class="links">
