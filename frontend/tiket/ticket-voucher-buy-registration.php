2bde2096fa49d7285879f7f79f0464b7ee39060b
Valian <valvilvul@gmail.com>
2017-06-08 23:32:40 +0700

    change voucher ticket views

--- frontend/tiket/ticket-voucher-buy-registration.php
+++ frontend/tiket/ticket-voucher-buy-registration.php
@@ -7,7 +7,7 @@
 				<div class="row">
 					<a href="/BK-Ganteng/frontend/tiket/" class="pull-right">Back To Home</a>
 					<div class="header col-md-12">
-						<h1 class="coming-soon" style="text-align:"center"; ">Aktivasi Voucher</h1>
+						<h1 class="head-form" style="text-align:"center"; ">Isi Data Peserta</h1>
 					</div>
 					<form method="post">
 						<p>Isilah identitas diri pada form di bawah ini</p>
@@ -31,26 +31,9 @@
 						    <label for="school">Asal Sekolah:</label>
 						    <input type="address" class="form-control" id="school">
 						</div>
-						<button type="button" value="ticketSubmitted" class="btn btn-success" data-toggle="modal" data-target="#myModal" name="submitTicket">Submit</button>
-						<div class="modal fade" id="myModal" role="dialog">
-						    <div class="modal-dialog">
-							    <!-- Modal content-->
-							    <div class="modal-content">
-							        <div class="modal-header">
-								        <button type="button" class="close" data-dismiss="modal">&times;</button>
-								        <h4 class="modal-title">Download Tiket</h4>
-							        </div>
-							        <div class="modal-body">
-							        	<a href="">
-							          		<button type="button" class="btn btn-default" value="ticketDownloaded">Download</button>
-							          	</a>
-							        </div>
-							        <div class="modal-footer">
-							          	<button type="button" class="btn btn-default" data-dismiss="modal">Tutup</button>
-							        </div>
-							    </div>
-						    </div>
-						</div>
+						<a href="download-tiket.php">
+							<button type="button" value="ticketSubmitted" class="btn btn-activate" name="submitTicket">Submit</button>
+						</a>
 					</form>
 				</div>
 			</div>
--- frontend/tiket/ticket-voucher-buy.php
+++ frontend/tiket/ticket-voucher-buy.php
@@ -7,15 +7,18 @@
 				<div class="row">
 					<a href="/BK-Ganteng/frontend/tiket/" class="pull-right">Back To Home</a>
 					<div class="col-md-12">
-						<h1 class="coming-soon">Isi Data Peserta</h1>
+						<h1 class="head-form">Aktivasi Voucher</h1>
 					</div>
 					<div method="post">
 						<p>Masukkan kode yang terdapat pada tiket voucher</p>
 						<form>
 							<div class="form-group">
 								<input type="text"  class="form-control" name="ticketVoucherNumber">
+							</div>
+
+							<div class="form-group">
 								<a href="ticket-voucher-buy-registration.php">
-									<button type="button" class="btn btn-success" value="TicketActivated" name="activateTicket">Aktivasi</button>
+									<button type="button" class="btn btn-activate" value="TicketActivated" name="activateTicket">Aktivasi</button>
 								</a>
 							</div>
 						</form>
