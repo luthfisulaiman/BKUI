383bb1d9a20f4e0d51209460e521a9277a009bb4
Hafizh Rafizal Adnan <hafizh.rafizal@office.ui.ac.id>
2017-06-16 04:27:50 +0700

    Update minor frontend. Payment page

--- TIKET/resources/views/pages/payment.blade.php
+++ TIKET/resources/views/pages/payment.blade.php
@@ -3,30 +3,18 @@
 	<div id="payment">
 	<div class="row">
 		<div class="col-md-12">
-		<h1 class="coming-soon">Panduan Pembayaran</h1>
-		<h3>Nomor Referensi {{ $pembayar->kode_bayar }}</h3>
+			<h1 class="coming-soon">Panduan Pembayaran</h1>
+			<h3>Nomor Referensi {{ $pembayar->kode_bayar }}</h3>
 		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-6">
+		<div class="col-md-12">
 			<h1 class="payment-head"><span class="num-rounded">1.</span> Lakukan Pembayaran Sebelum</h1>
 			<div class="thumbnail">
 				<div class="modal-body">
 					@if (isset($pembayar->waktu_bayar))
-						<h4>{{ $pembayar->waktu_bayar }}</h4>
+						<h4 class="text-center">{{ $pembayar->waktu_bayar }}</h4>
 					@endif
 				</div>
 			</div>	
-			<h1 class="payment-head"><span class="num-rounded">3.</span> Konfirmasi Pembayaran</h1>
-			<div class="thumbnail">
-				<div class="modal-body">
-					<p>E-Tiket akan dikirim maksimal 3 hari setelah pembayaran</p>
-				<form method="post" action="../public/payment">
-					{{ csrf_field() }}
-					<input type="submit" class="btn btn-activate" value="Saya sudah membayar" name="submit">
-				</form>
-				</div>	
-			</div>
 		</div>
 		<div class="col-md-6">
 			<h1 class="payment-head"><span class="num-rounded">2.</span> Transfer Ke</h1>
@@ -51,6 +39,19 @@
 		      	</div>
 		     
 			</div>
+	</div>
+	<div class="col-md-6">
+		<h1 class="payment-head"><span class="num-rounded">3.</span> Konfirmasi Pembayaran</h1>
+			<div class="thumbnail">
+				<div class="modal-body">
+					<p>E-Tiket akan dikirim maksimal 3 hari setelah pembayaran</p>
+				<form method="post" action="../public/payment">
+					{{ csrf_field() }}
+					<input type="submit" class="btn btn-activate" value="Saya sudah membayar" name="submit">
+				</form>
+				</div>	
+			</div>
+	</div>
 	</div>
 </div>
 
