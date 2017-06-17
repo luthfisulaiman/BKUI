a18d93536975acfe258f0019a003551383246c8f
dwi.nanda09 <dwi.nanda@ui.ac.id>
2017-06-17 10:55:03 +0700

    tiket pdf kuy

---
+++ frontend/tiket/IPA_pdf_B.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="pdf.css"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+
+  <div class="main">
+    <h1 id="bc">BARCODE</h1>
+    <h1 id="name">NAMA NAMA NAMA</h1>
+    <h1 id="kode_tiket">0123456789</h1>
+    <img src="ready_pdf/IPA/ipaB.png" alt="kosong-A">
+    <!-- <div id="barcode_box">
+    </div>
+    <div id="name_box">
+      NAME
+    </div> -->
+  </div>
+
+</body>
+</html>
